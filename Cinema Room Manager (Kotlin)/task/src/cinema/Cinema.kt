0d452486e1dd59fe8e68082dd23cd75996da2dad
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-09-27 22:45:12 +0100

    stage 3

--- Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
@@ -6,23 +6,38 @@ fun main() {
     println("Enter the number of seats in each row:")
     val seatsPerRow = readln().toInt()
     val cinemaScreen = CinemaScreen(rows, seatsPerRow)
-    println("Total income:")
-    println("$${ScreenCalculator(cinemaScreen).profit()}")
+    println("\n$cinemaScreen\n")
+    println("Enter a row number:")
+    val bookingRow = readln().toInt()
+    println("Enter a seat number in that row:")
+    val bookingSeat = readln().toInt()
+    cinemaScreen.bookSeat(bookingRow, bookingSeat)
+    println("\nTicket price: $${ScreenCalculator(cinemaScreen).cost(bookingRow)}\n")
+    println(cinemaScreen)
 }
 
 data class CinemaScreen(val rows: Int, val seatsPerRow: Int) {
+    private val seats = Array(rows) {CharArray(seatsPerRow) {'S'} }
     val totalSeats = rows * seatsPerRow
+
+    fun bookSeat(row: Int, seatNum: Int) {
+        seats[row - 1][seatNum - 1] = 'B'
+    }
+
+    override fun toString(): String {
+        return "Cinema:\n" +
+                (1..seatsPerRow).joinToString(" ", prefix = "  ", postfix = "\n") +
+                seats.indices.joinToString("\n") { "${it + 1} ${seats[it].joinToString(" ")}" }
+    }
 }
 
 class ScreenCalculator(private val screen: CinemaScreen){
-    fun profit(): Int {
+    fun cost(seatRow: Int): Int {
         return if (screen.totalSeats <= 60) {
-            screen.totalSeats * 10
+            10
         } else {
             val frontRowsCount = screen.rows / 2
-            val backRowsCount = screen.rows - frontRowsCount
-
-            frontRowsCount * screen.seatsPerRow * 10 + backRowsCount * screen.seatsPerRow * 8
+            if (seatRow <= frontRowsCount) 10 else 8
         }
     }
 }
